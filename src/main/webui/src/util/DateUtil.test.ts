fe455d0eec907a79b9772c116d76a12301ed9d3c
Pascal Euhus <pascal.euhus@gmx.de>
2023-10-23 21:19:35 +0200

    fix frontend tests

--- src/main/webui/src/util/DateUtil.test.ts
+++ src/main/webui/src/util/DateUtil.test.ts
@@ -6,7 +6,7 @@ describe("DateUtil", () => {
   it("should format date and time with trailing zeros", () => {
     const dateString = "2023-01-01T01:01:54.000Z";
     const formattedDateTime = formatDateTime(dateString);
-    expect(formattedDateTime).toStrictEqual("01-01-2023 02:01");
+    expect(formattedDateTime).toStrictEqual("01-01-2023 01:01");
   });
 
   it("should format date", () => {
@@ -16,6 +16,6 @@ describe("DateUtil", () => {
 
   it("should format date and time", () => {
     const formattedDateTime = formatDateTime(dateString);
-    expect(formattedDateTime).toStrictEqual("01-01-2023 16:11");
+    expect(formattedDateTime).toStrictEqual("01-01-2023 15:11");
   });
 });
