4a0ec4c6e5abacecbef6e74c207aec05bd60d5c5
jonasz-lasut <jonasz.lasut@gmail.com>
2024-01-18 17:05:14 +0100

    add basic PR pipeline to lint and check docs

---
+++ ci/helm/docs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Reference: https://github.com/norwoodj/helm-docs
+set -eux
+REPO_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+echo "$REPO_ROOT"
+
+echo "Running Helm-Docs"
+docker run \
+    -v "$REPO_ROOT:/helm-docs" \
+    -u $(id -u) \
+    jnorwood/helm-docs:v1.9.1
