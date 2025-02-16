55a26dfbbaf84fc99f7d1a44bbb96ab8bb8b40a0
Pascal Euhus <pascal.euhus@gmx.de>
2025-02-16 20:37:36 +0100

    fix version enforcement for modules and providers

--- src/main/java/core/service/VersionService.java
+++ src/main/java/core/service/VersionService.java
@@ -7,8 +7,8 @@ public class VersionService {
   // see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
   private static final String SEMVER_PATTERN_TEMPLATE = "^%s(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$";
 
-  private static final Pattern moduleSemVerPattern = Pattern.compile(String.format(SEMVER_PATTERN_TEMPLATE, "(v?)"));
-  private static final Pattern providerSemVerPattern = Pattern.compile(String.format(SEMVER_PATTERN_TEMPLATE, "v"));
+  private static final Pattern moduleSemVerPattern = Pattern.compile(String.format(SEMVER_PATTERN_TEMPLATE, ""));
+  private static final Pattern providerSemVerPattern = Pattern.compile(String.format(SEMVER_PATTERN_TEMPLATE, ""));
 
   public static boolean isValidModuleVersion(String version) {
     return moduleSemVerPattern.matcher(version).matches();
--- src/test/java/core/service/VersionServiceTest.java
+++ src/test/java/core/service/VersionServiceTest.java
@@ -8,21 +8,21 @@ class VersionServiceTest {
   @Test
   void isValidModuleVersion() {
     assertTrue(VersionService.isValidModuleVersion("1.0.0"));
-    assertTrue(VersionService.isValidModuleVersion("v1.0.0"));
     assertTrue(VersionService.isValidModuleVersion("2.1.3-alpha"));
     assertTrue(VersionService.isValidModuleVersion("0.0.1-beta+exp.sha.5114f85"));
     assertTrue(VersionService.isValidModuleVersion("1.0.0+20130313144700"));
+    assertFalse(VersionService.isValidModuleVersion("v1.0.0"));
     assertFalse(VersionService.isValidModuleVersion("1.0"));
     assertFalse(VersionService.isValidModuleVersion("1.Foo.0"));
   }
 
   @Test
   void isValidProviderVersion() {
-    assertTrue(VersionService.isValidProviderVersion("v1.0.0"));
-    assertTrue(VersionService.isValidProviderVersion("v2.1.3-alpha"));
-    assertTrue(VersionService.isValidProviderVersion("v0.0.1-beta+exp.sha.5114f85"));
-    assertFalse(VersionService.isValidProviderVersion("1.0.0"));
-    assertFalse(VersionService.isValidProviderVersion("1.0.0+20130313144700"));
+    assertTrue(VersionService.isValidProviderVersion("1.0.0"));
+    assertTrue(VersionService.isValidProviderVersion("2.1.3-alpha"));
+    assertTrue(VersionService.isValidProviderVersion("0.0.1-beta+exp.sha.5114f85"));
+    assertTrue(VersionService.isValidProviderVersion("1.0.0+20130313144700"));
+    assertFalse(VersionService.isValidProviderVersion("v1.0.0"));
     assertFalse(VersionService.isValidProviderVersion("v1.0"));
     assertFalse(VersionService.isValidProviderVersion("v1.Foo.0"));
   }
