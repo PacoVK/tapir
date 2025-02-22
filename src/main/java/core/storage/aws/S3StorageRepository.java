ed74eb4227500540ae95e07ea33a8f50ab3b17bb
Pascal Euhus <pascal.euhus@gmx.de>
2025-02-22 22:26:03 +0100

    Revert "feat: allow to use minio as an aws s3 provider"
    This reverts commit c999bba5f9760ecabbaa5f32113b1528ce952caa.

--- src/main/java/core/storage/aws/S3StorageRepository.java
+++ src/main/java/core/storage/aws/S3StorageRepository.java
@@ -7,23 +7,15 @@ import core.terraform.Module;
 import core.terraform.Provider;
 import core.upload.FormData;
 import io.quarkus.arc.lookup.LookupIfProperty;
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.logging.Logger;
-import java.util.Optional;
 import java.util.stream.Stream;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3ClientBuilder;
-import software.amazon.awssdk.services.s3.S3Configuration;
-import software.amazon.awssdk.services.s3.endpoints.S3EndpointParams;
-import software.amazon.awssdk.services.s3.endpoints.S3EndpointProvider;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -41,45 +33,11 @@ public class S3StorageRepository extends StorageRepository {
 
   @ConfigProperty(name = "registry.storage.s3.bucket.name")
   String bucketName;
-  @ConfigProperty(name = "registry.storage.s3.custom_endpoint")
-  Optional<String> customEndpoint;
-  @ConfigProperty(name = "registry.storage.s3.use_path_style_access", defaultValue = "false")
-  boolean usePathStyleAccess;
 
-  @PostConstruct
-  public void initialize() {
-    var region = DefaultAwsRegionProviderChain.builder().build().getRegion();
-    var credentials = DefaultCredentialsProvider.create();
-    S3ClientBuilder s3ClientBuilder = S3Client.builder()
-      .credentialsProvider(credentials)
-      .region(region);
-    var s3PresignerBuilder = S3Presigner.builder()
-      .credentialsProvider(credentials)
-      .region(region);
-    if (!customEndpoint.isEmpty()) {
-      var endpointParams = S3EndpointParams.builder()
-        .endpoint(customEndpoint.get())
-        .region(region)
-        .build();
 
-      var endpoint = S3EndpointProvider
-        .defaultProvider()
-        .resolveEndpoint(endpointParams).join();
-
-      s3ClientBuilder.endpointOverride(endpoint.url());
-      s3PresignerBuilder.endpointOverride(endpoint.url());
-    }
-
-    if (usePathStyleAccess) {
-      s3ClientBuilder.forcePathStyle(true);
-      s3PresignerBuilder.serviceConfiguration(
-        S3Configuration.builder().pathStyleAccessEnabled(true).build()
-      );
-    }
-
-    this.s3 = s3ClientBuilder.build();
-    this.presigner = s3PresignerBuilder.build();
-
+  public S3StorageRepository(S3Client s3, S3Presigner presigner) {
+    this.s3 = s3;
+    this.presigner = presigner;
   }
 
   @Override
